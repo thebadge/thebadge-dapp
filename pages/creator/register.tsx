0602d21e847a03351c486533993080e7a3083997
Fernando Ramirez <fernando.ramirez@diva-e.com>
2023-02-06 15:37:44 +0100

    fix ipfs upload

--- pages/creator/register.tsx
+++ pages/creator/register.tsx
@@ -40,9 +40,8 @@ const Register: NextPageWithLayout = () => {
     if (!address) {
       throw Error('Web3 address not provided')
     }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const uploadedInfo = await ipfsUpload({ attributes: JSON.stringify(data), filePaths: ['logo'] })
+
+    const uploadedInfo = await ipfsUpload({ attributes: data, filePaths: ['logo'] })
 
     const transaction = await sendTx(() =>
       theBadge.registerEmitter(address, `ipfs://${uploadedInfo.result?.ipfsHash}`),
