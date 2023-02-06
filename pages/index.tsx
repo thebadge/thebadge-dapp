0d5ad4a8061b865c0e5d6b7fc1eea842cb2e16a3
Fernando Ramirez <fernando.ramirez@diva-e.com>
2023-02-06 12:11:57 +0100

    Advance with home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react'
 
-import { Box, Typography, styled } from '@mui/material'
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined'
+import { Box, styled, Typography } from "@mui/material";
 
 import { NextPageWithLayout } from '@/pages/_app'
 import { DefaultLayout } from '@/src/components/layout/DefaultLayout'
@@ -13,9 +14,32 @@ const Address: React.FC = () => {
   return address ? <Typography variant="title1">{address}</Typography> : null
 }
 
-const CenteredText = styled(Typography)(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'center',
+const CertificationProcess: React.FC = () => {
+  return (
+    <div>
+      <Typography
+        alignItems={'center'}
+        component="span"
+        display={'flex'}
+        fontSize={12}
+        fontWeight={900}
+        justifyContent={'center'}
+        lineHeight={'15px'}
+        marginBottom={10}
+      >
+        FULL CERTIFICATION PROCESS TUTORIAL
+        <ArrowForwardIosOutlinedIcon />
+      </Typography>
+    </div>
+  )
+}
+
+const Section = styled('div')(({}) => ({
+  border: '1px solid #FFFFFF',
+  boxShadow: '0px 0px 6px rgba(0, 0, 0, 0.3)',
+  borderRadius: '15px',
+  minHeight: '100px',
+  width: '100%',
 }))
 
 const Home: NextPageWithLayout = () => {
@@ -23,17 +47,23 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <Box display="flex" flexDirection="column" ref={homeSection}>
-      <CenteredText variant="h2">Welcome to The Badge!</CenteredText>
-      <CenteredText variant="h4">DECENTRALIZED CERTIFICATION PLATFORM</CenteredText>
-      <Box display="flex" flexDirection="column">
-        {/*<LinkWithTranslation pathname="/creator/register">1. Register Creator</LinkWithTranslation>*/}
-        {/*<LinkWithTranslation pathname="/curator/register">2. Register Curator</LinkWithTranslation>*/}
+      <Typography component="h2" marginBottom={2} textAlign="center" variant="h2">
+        Welcome to TheBadge!
+      </Typography>
+      <Typography component="div" marginBottom={10} textAlign="center" variant="h5">
+        DECENTRALIZED CERTIFICATION PLATFORM
+      </Typography>
+      <CertificationProcess />
+      <Section>Claim one of these badges</Section>
+      {/*<Box display="flex" flexDirection="column">*/}
+      {/*<LinkWithTranslation pathname="/creator/register">1. Register Creator</LinkWithTranslation>*/}
+      {/*<LinkWithTranslation pathname="/curator/register">2. Register Curator</LinkWithTranslation>*/}
 
-        {/*<LinkWithTranslation pathname="/badge/type/create">*/}
-        {/*  3. Create badge-type*/}
-        {/*</LinkWithTranslation>*/}
-        {/*<LinkWithTranslation pathname="/badge/mint">4. Mint badge</LinkWithTranslation>*/}
-      </Box>
+      {/*<LinkWithTranslation pathname="/badge/type/create">*/}
+      {/*  3. Create badge-type*/}
+      {/*</LinkWithTranslation>*/}
+      {/*<LinkWithTranslation pathname="/badge/mint">4. Mint badge</LinkWithTranslation>*/}
+      {/*</Box>*/}
     </Box>
   )
 }
