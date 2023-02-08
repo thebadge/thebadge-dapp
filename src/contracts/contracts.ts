feda390f278275ff65fba5e47d41ab9e7195ce0a
nicolas <nicosampler@users.noreply.github.com>
2023-02-08 15:04:56 -0300

    fix: changes to make the new contracts work (#18)
    Add badgeType challenge duration

--- src/contracts/contracts.ts
+++ src/contracts/contracts.ts
@@ -1,6 +1,6 @@
-import ERC20 from './abis/ERC20.json'
 import Kleros from './abis/Kleros.json'
 import KlerosBadgeTypeController from './abis/KlerosBadgeTypeController.json'
+import TCR from './abis/TCR.json'
 import TheBadge from './abis/TheBadge.json'
 import { Chains } from '@/src/config/web3'
 
@@ -23,19 +23,11 @@ export const contracts = {
     },
     abi: Kleros,
   },
-  DAI: {
+  TCR: {
     address: {
-      // [Chains.mainnet]: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-      [Chains.goerli]: '0x5c221e77624690fff6dd741493d735a17716c26b',
+      [Chains.goerli]: null,
     },
-    abi: ERC20,
-  },
-  USDC: {
-    address: {
-      // [Chains.mainnet]: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-      [Chains.goerli]: '0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86',
-    },
-    abi: ERC20,
+    abi: TCR,
   },
 } as const
 
--- src/hooks/useContractInstance.ts
+++ src/hooks/useContractInstance.ts
@@ -1,6 +1,3 @@
-import { JsonRpcSigner } from '@ethersproject/providers'
-import nullthrows from 'nullthrows'
-
 import { ContractsKeys, contracts } from '@/src/contracts/contracts'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
 import * as typechainImports from '@/types/generated/typechain'
@@ -13,11 +10,13 @@ type AppFactories = GetFactories<ObjectValues<typeof typechainImports>>
 export const useContractInstance = <F extends AppFactories, RT extends ReturnType<F['connect']>>(
   contractFactory: F,
   contractKey: ContractsKeys,
+  address?: string,
 ) => {
   const { appChainId, readOnlyAppProvider, web3Provider } = useWeb3Connection()
-  const address = contracts[contractKey]['address'][appChainId]
+  const _address = address ? address : contracts[contractKey]['address'][appChainId]
+  if (!_address) throw `Address for ${contractKey} and ${appChainId} is null`
   const signer = web3Provider?.getSigner() || readOnlyAppProvider
-  nullthrows(signer, 'There is not signer to execute a tx.')
+  if (!signer) throw 'There is not signer to execute a tx.'
 
-  return contractFactory.connect(address, signer as JsonRpcSigner) as RT
+  return contractFactory.connect(_address, signer) as RT
 }
