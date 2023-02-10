173d00d45e475d17fcfaacfedb028d74c4548441
Agustín Longoni <agustin.longoni@altoros.com>
2023-02-10 10:24:05 -0300

    move svg and add them as a svg

--- src/pagePartials/home/CertificationProcess.tsx
+++ src/pagePartials/home/CertificationProcess.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined'
 import { Box, Typography } from '@mui/material'
+
+import { Choose } from './svg/choose'
+import { Complete } from './svg/complete'
+import { Evidence } from './svg/evidence'
 
 export default function CertificationProcess() {
   return (
     <div>
       <Box display={'flex'} flexDirection={'row'} gap={8} justifyContent={'center'} mb={4}>
-        <Image alt="Choose your certification" height="73" src="shareable/choose.svg" width="116" />
-        <Image alt="Upload the evidence" height="73" src="shareable/evidence.svg" width="116" />
-        <Image alt="Receive your Badge" height="73" src="shareable/complete.svg" width="116" />
+        <Choose />
+        <Evidence />
+        <Complete />
       </Box>
       <Typography
         alignItems={'center'}
