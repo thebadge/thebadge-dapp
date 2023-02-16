184d9a94e7a441bda89974d6bc32abb08a7115d8
Agustín Longoni <agustin.longoni@altoros.com>
2023-02-16 12:46:37 -0300

    fix colors

--- src/components/header/ConnectWalletButton.tsx
+++ src/components/header/ConnectWalletButton.tsx
@@ -1,8 +1,9 @@
 import { Button, ButtonProps, styled } from '@mui/material'
+import { colors } from 'thebadge-ui-library'
 
 const StyledButton = styled(Button)(({ theme }) => ({
   textTransform: 'none',
-  color: theme.palette.text.secondary,
+  color: colors.black,
   borderRadius: theme.spacing(1),
 }))
 export default function ConnectWalletButton({ children, ...rest }: ButtonProps) {
