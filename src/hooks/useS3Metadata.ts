52755657f2937de44df14d5235474fcde8479581
Agustin Pane <agustin.pane@gmail.com>
2023-03-10 15:41:51 +0100

    Add debug logs

--- src/hooks/useS3Metadata.ts
+++ src/hooks/useS3Metadata.ts
@@ -6,9 +6,11 @@ import { BackendResponse } from '@/types/utils'
 
 export default function useS3Metadata<T>(hash: string) {
   return useSWR(hash.length ? hash : null, async (_hash) => {
+    console.log(`Loading backend URL...: ${BACKEND_URL}`)
     const cleanedHash = (_hash as string).replace(/^ipfs?:\/\//, '')
-
-    const res = await axios.get<BackendResponse<T>>(`${BACKEND_URL}/api/ipfs/${cleanedHash}`)
+    const requestUrl = `${BACKEND_URL}/api/ipfs/${cleanedHash}`
+    console.log(`Requesting to s3Metadata to url: `, requestUrl)
+    const res = await axios.get<BackendResponse<T>>(requestUrl)
     return res.data.result
   })
 }
