af3d054c8d2a948ba262348870fafc134524e287
Federico Madoery <fede.madoery@hotmail.com>
2023-04-05 11:24:24 -0300

    feat: Add privacy policy (#48)
    Remove the old folder with files
    Add privacy-policy.tsx
    Add the ability to render pages with markdown

---
+++ pages/legal/privacy-policy.tsx
@@ -0,0 +1,39 @@
+import { MDXProvider } from '@mdx-js/react'
+import { Container, ListItem, MenuList, Typography } from '@mui/material'
+import { MDXComponents } from 'mdx/types'
+
+import PrivacyPolicyMd from '@/markdown/en/privacy.md'
+
+const components: MDXComponents = {
+  h1: (props: any) => <Typography {...props} component="h1" variant="h2" />,
+  h2: (props: any) => <Typography {...props} component="h2" mt={2} variant="h5" />,
+  h3: (props: any) => <Typography {...props} component="h3" mt={2} variant="h6" />,
+  h4: (props: any) => <Typography {...props} component="h4" mt={2} variant="subtitle2" />,
+  h6: (props: any) => (
+    <Typography
+      {...props}
+      component="p"
+      mt={2}
+      sx={{ textDecoration: 'underline' }}
+      variant="subtitle2"
+    />
+  ),
+  p: (props: any) => <Typography {...props} component="p" mt={2} variant="body4" />,
+  li: (props: any) => (
+    <ListItem>
+      <Typography {...props} variant="body4" />
+    </ListItem>
+  ),
+  ol: (props: any) => <MenuList {...props} />,
+  ul: (props: any) => <MenuList {...props} />,
+}
+
+export default function PrivacyPolicy() {
+  return (
+    <Container fixed>
+      <MDXProvider components={components}>
+        <PrivacyPolicyMd />
+      </MDXProvider>
+    </Container>
+  )
+}
