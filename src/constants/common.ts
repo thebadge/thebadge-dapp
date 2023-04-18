839b7611f7e6ef00d95a4afdb5dcde84c60ce415
Federico Madoery <fede.madoery@hotmail.com>
2023-04-18 15:55:59 -0300

    feat: Improve metadata (#53)
    Update ui-lib
    Add metadata info
    Use APP_URL env var to config the metadata

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,6 @@
 export const appName = process.env.NEXT_PUBLIC_APP_NAME || 'letsHopeWeCanAvoidNameClashingThen'
+export const appTier = process.env.NEXT_PUBLIC_APP_TIER || 'staging'
+
 export const cookiesWarningEnabled =
   process.env.NEXT_PUBLIC_COOKIES_WARNING_ENABLED === 'true' || ''
 export const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''
