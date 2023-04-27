525ab521efd82bede8a0414a62b2e2d0c342626b
Fernando Ramirez <fernando.ramirez@diva-e.com>
2023-04-27 17:49:40 +0200

    Fix expiration time on create badge

--- src/components/form/ExpirationField.tsx
+++ src/components/form/ExpirationField.tsx
@@ -24,6 +24,7 @@ import { FormField } from '@/src/components/form/helpers/FormField'
 import { ExpirationTypeSchema } from '@/src/components/form/helpers/customSchemas'
 import { convertToFieldError } from '@/src/components/form/helpers/validators'
 
+type TimeUnit = 'day' | 'month' | 'year'
 const options = ['day', 'month', 'year']
 
 const Wrapper = styled(Box)(() => ({
@@ -52,7 +53,7 @@ export function ExpirationField({
   const [stringValue, setStringValue] = useState<string>('')
   const [enableExpiration, setEnableExpiration] = useState<boolean>(false)
 
-  const [unit, setUnit] = useState<'day' | 'month' | 'year'>('day')
+  const [unit, setUnit] = useState<TimeUnit>('day')
 
   useEffect(() => {
     // Easy way to set default value
@@ -60,84 +61,97 @@ export function ExpirationField({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  function setExpiration(value: string) {
+    setStringValue(value)
+    onChange(parseToSeconds(value, unit))
+  }
+
   function handleOnChange(e: any) {
     const cleanValue = e.target.value.replace(/[^0-9.]/g, '') || '0'
     if (/^-?\d+(?:\.\d*)?$/.test(cleanValue)) {
-      setStringValue(cleanValue)
-      onChange(parseToSeconds(cleanValue, unit))
+      setExpiration(cleanValue)
     }
   }
 
-  function handleDropdownChange(e: SelectChangeEvent) {
+  function handleDropdownChange(e: SelectChangeEvent<TimeUnit>) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    setUnit(e.target.value)
+    const newUnit: TimeUnit = e.target.value
+    setUnit(newUnit)
+    onChange(parseToSeconds(stringValue, newUnit))
+  }
+
+  function toggleExpiration(enable: boolean) {
+    setEnableExpiration(enable)
+    if (!enable) {
+      onChange(0) // if disable expiration, reset the values
+      setExpiration('0')
+      setUnit('day')
+    }
   }
 
   return (
     <FormField
       formControl={
-        <Wrapper>
-          {enableExpiration && (
-            <>
-              <TextField
-                InputProps={{
-                  endAdornment: (
-                    <Tooltip
-                      arrow
-                      title={
-                        placeholder +
-                        `\n e.g. If you mint this badge today, It will expire on: ${validTo(value)}`
-                      }
-                    >
-                      <InfoOutlinedIcon />
-                    </Tooltip>
-                  ),
-                }}
-                color="secondary"
-                label={label}
-                onChange={handleOnChange}
-                sx={{ justifyContent: 'end' }}
-                value={stringValue}
-                variant={'standard'}
-              />
-              <MUISelect
-                id="unit-select"
-                onChange={handleDropdownChange}
-                size="small"
-                sx={{ textTransform: 'capitalize', ml: 2 }}
-                value={unit || ''}
-                variant="standard"
-              >
-                {options.map((op) => {
-                  return (
-                    <MenuItem key={op} sx={{ textTransform: 'capitalize' }} value={op}>
-                      {op}s
-                    </MenuItem>
-                  )
-                })}
-              </MUISelect>
-            </>
-          )}
-          {!enableExpiration && (
-            <Box alignItems="center" display="flex">
-              <FormControlLabel
-                control={
-                  <MUICheckbox
-                    checked={enableExpiration}
-                    onChange={() => setEnableExpiration(true)}
-                    sx={{ width: 'fit-content' }}
-                  />
-                }
-                label={<Typography>Enable expiration time</Typography>}
-                sx={{ marginTop: 2 }}
-              />
-              <Tooltip arrow title={placeholder}>
-                <InfoOutlinedIcon sx={{ marginLeft: 1 }} />
-              </Tooltip>
-            </Box>
-          )}
-        </Wrapper>
+        <div>
+          <Box alignItems="center" display="flex">
+            <FormControlLabel
+              control={
+                <MUICheckbox
+                  checked={enableExpiration}
+                  onChange={() => toggleExpiration(!enableExpiration)}
+                  sx={{ width: 'fit-content' }}
+                />
+              }
+              label={<Typography>Enable expiration time</Typography>}
+              sx={{ my: 1 }}
+            />
+          </Box>
+
+          <Wrapper>
+            {enableExpiration && (
+              <>
+                <TextField
+                  InputProps={{
+                    endAdornment: (
+                      <Tooltip
+                        arrow
+                        title={
+                          placeholder +
+                          `\n e.g. If you mint this badge today, It will expire on: ${validTo(value)}`
+                        }
+                      >
+                        <InfoOutlinedIcon />
+                      </Tooltip>
+                    ),
+                  }}
+                  color="secondary"
+                  label={label}
+                  onChange={handleOnChange}
+                  sx={{ justifyContent: 'end' }}
+                  value={stringValue}
+                  variant={'standard'}
+                />
+                <MUISelect
+                  id="unit-select"
+                  onChange={handleDropdownChange}
+                  size="small"
+                  sx={{ textTransform: 'capitalize', ml: 2 }}
+                  value={unit || ''}
+                  variant="filled"
+                >
+                  {options.map((op) => {
+                    return (
+                      <MenuItem key={op} sx={{ textTransform: 'capitalize' }} value={op}>
+                        {op}s
+                      </MenuItem>
+                    )
+                  })}
+                </MUISelect>
+              </>
+            )}
+          </Wrapper>
+        </div>
       }
       labelPosition={'top'}
       status={error ? TextFieldStatus.error : TextFieldStatus.success}
@@ -174,7 +188,7 @@ function validTo(days?: z.infer<typeof ExpirationTypeSchema> | undefined) {
   return dayjs().add(days, 'seconds').format('DD/MM/YYYY')
 }
 
-function parseToSeconds(value: string, unit: 'day' | 'month' | 'year') {
+function parseToSeconds(value: string, unit: TimeUnit) {
   switch (unit) {
     case 'day':
       return Number(value) * 24 * 60 * 60 // Each day has 24hs and each hour has 60 min, each min has 60 seconds
