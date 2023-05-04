d67c00afdc3d74e149f422639961b8888f2b82c0
Federico Madoery <fede.madoery@hotmail.com>
2023-05-04 13:40:11 -0300

    Remove PDF worker

--- next.config.mjs
+++ next.config.mjs
@@ -45,18 +45,6 @@ export default withMDX(withBundleAnalyzerWrapper(
         "ethereum-optimism.github.io",
         "assets.coingecko.com"
       ]
-    },
-    webpack: (config) => {
-      // load worker files as a urls by using Asset Modules
-      // https://webpack.js.org/guides/asset-modules/
-      config.module.rules.unshift({
-        test: /pdf\.worker\.(min\.)?js/,
-        type: "asset/resource",
-        generator: {
-          filename: "static/worker/[hash][ext][query]"
-        }
-      });
-      return config;
     }
   }),
 ));
--- pdf-worker.js
+++
@@ -1,6 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  // use minified version for production
-  module.exports = require('pdfjs-dist/build/pdf.worker.min.js')
-} else {
-  module.exports = require('pdfjs-dist/build/pdf.worker.js')
-}
