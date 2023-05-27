51304fe5c8a5b7bef103f462a6f6cd8bb7a7be94
Federico Madoery <fede.madoery@hotmail.com>
2023-05-27 17:43:24 -0300

    Use useCallbacks on useDate

--- src/hooks/useDate.tsx
+++ src/hooks/useDate.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+
 import { useTranslation } from 'next-export-i18n'
 
 export interface TimeLeft {
@@ -14,57 +16,60 @@ export const MS_PER_DAY = MS_PER_HOUR * 24
 export const useDate = () => {
   const { t } = useTranslation()
 
-  const timestampToDate = (timestamp: number): Date => {
+  const timestampToDate = useCallback((timestamp: number): Date => {
     return new Date(timestamp * 1000)
-  }
+  }, [])
 
-  const getTimeLeft = (date: Date): TimeLeft => {
-    const now = new Date().getTime()
-    const timeLeft = date.getTime() - now
+  const getTimeLeft = useCallback(
+    (date: Date): TimeLeft => {
+      const now = new Date().getTime()
+      const timeLeft = date.getTime() - now
 
-    // calculate remaining time
-    const days = Math.floor(timeLeft / MS_PER_DAY)
-    const hours = Math.floor((timeLeft % MS_PER_DAY) / MS_PER_HOUR)
-    const minutes = Math.floor((timeLeft % MS_PER_HOUR) / MS_PER_MINUTE)
-    const seconds = Math.floor((timeLeft % MS_PER_MINUTE) / MS_PER_SECOND)
+      // calculate remaining time
+      const days = Math.floor(timeLeft / MS_PER_DAY)
+      const hours = Math.floor((timeLeft % MS_PER_DAY) / MS_PER_HOUR)
+      const minutes = Math.floor((timeLeft % MS_PER_HOUR) / MS_PER_MINUTE)
+      const seconds = Math.floor((timeLeft % MS_PER_MINUTE) / MS_PER_SECOND)
 
-    if (days > 0) {
-      return {
-        quantity: days,
-        unitText: t('date.timeLeft.days'),
+      if (days > 0) {
+        return {
+          quantity: days,
+          unitText: t('date.timeLeft.days'),
+        }
+      } else if (hours > 0) {
+        return {
+          quantity: hours,
+          unitText: t('date.timeLeft.hours'),
+        }
+      } else if (minutes > 0) {
+        return {
+          quantity: minutes,
+          unitText: t('date.timeLeft.minutes'),
+        }
+      } else if (seconds > 0) {
+        return {
+          quantity: seconds,
+          unitText: t('date.timeLeft.seconds'),
+        }
+      } else {
+        return {
+          quantity: 0,
+          unitText: t('date.timeLeft.time'),
+        }
       }
-    } else if (hours > 0) {
-      return {
-        quantity: hours,
-        unitText: t('date.timeLeft.hours'),
-      }
-    } else if (minutes > 0) {
-      return {
-        quantity: minutes,
-        unitText: t('date.timeLeft.minutes'),
-      }
-    } else if (seconds > 0) {
-      return {
-        quantity: seconds,
-        unitText: t('date.timeLeft.seconds'),
-      }
-    } else {
-      return {
-        quantity: 0,
-        unitText: t('date.timeLeft.time'),
-      }
-    }
-  }
+    },
+    [t],
+  )
 
-  const getPendingTimeProgressPercentage = (
-    dueDate: Date,
-    pendingTimeDurationSeconds: number,
-  ): number => {
-    const now = new Date().getTime()
-    const timeLeftInSeconds = (dueDate.getTime() - now) / MS_PER_SECOND
-    const secondsOfProgressDone = pendingTimeDurationSeconds - timeLeftInSeconds
-    return Math.floor((100 / pendingTimeDurationSeconds) * secondsOfProgressDone)
-  }
+  const getPendingTimeProgressPercentage = useCallback(
+    (dueDate: Date, pendingTimeDurationSeconds: number): number => {
+      const now = new Date().getTime()
+      const timeLeftInSeconds = (dueDate.getTime() - now) / MS_PER_SECOND
+      const secondsOfProgressDone = pendingTimeDurationSeconds - timeLeftInSeconds
+      return Math.floor((100 / pendingTimeDurationSeconds) * secondsOfProgressDone)
+    },
+    [],
+  )
 
   return {
     timestampToDate,
