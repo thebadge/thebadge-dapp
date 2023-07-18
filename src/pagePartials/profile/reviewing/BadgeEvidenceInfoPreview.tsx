f35e614ad221d077f1b05b9442935a2aec060af4
Federico Madoery <fede.madoery@hotmail.com>
2023-07-18 18:34:10 -0300

    Add BadgeEvidenceInfoPreview.tsx on profile badgesIAmReviewing

---
+++ src/pagePartials/profile/reviewing/BadgeEvidenceInfoPreview.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+
+import { Box, Divider, Stack, Typography } from '@mui/material'
+import { ButtonV2, colors } from '@thebadge/ui-library'
+import { useTranslation } from 'next-export-i18n'
+
+import DisplayEvidenceField from '@/src/components/displayEvidence/DisplayEvidenceField'
+import { Address } from '@/src/components/helpers/Address'
+import SafeSuspense from '@/src/components/helpers/SafeSuspense'
+import { useEvidenceBadgeKlerosMetadata } from '@/src/hooks/subgraph/useBadgeKlerosMetadata'
+import { ListingCriteriaPreview } from '@/src/pagePartials/badge/explorer/ListingCriteriaPreview'
+import { useCurateProvider } from '@/src/providers/curateProvider'
+import { getEvidenceValue } from '@/src/utils/kleros/getEvidenceValue'
+import { Badge } from '@/types/generated/subgraph'
+
+export default function BadgeReviewingInfoPreview({ badge }: { badge: Badge }) {
+  const { t } = useTranslation()
+  const { challenge } = useCurateProvider()
+
+  const badgeKlerosMetadata = useEvidenceBadgeKlerosMetadata(badge?.id)
+  const badgeEvidence = badgeKlerosMetadata.data?.requestBadgeEvidence
+
+  if (!badgeEvidence || !badgeKlerosMetadata.data?.requestBadgeEvidenceRawUrl) {
+    throw 'There was an error fetching the badge evidence, try again in some minutes.'
+  }
+
+  // TODO @fede Change this data to show just the evidence of the initialized request
+  return (
+    <Stack gap={4}>
+      {/* Badge Receiver Address + Raw evidence info */}
+
+      <Box alignContent="center" display="flex" flex={1} justifyContent="space-between">
+        <Stack gap={1}>
+          <Typography fontSize={14} variant="body4">
+            {t('explorer.curate.curationList')}
+          </Typography>
+          <Address address={badge?.badgeModel.badgeModelKleros?.tcrList} />
+        </Stack>
+        <Box alignItems="flex-end" display="flex">
+          <Typography fontSize={14} variant="body4">
+            {`${t('explorer.curate.badgeId')} ${badge?.id}`}
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box alignContent="center" display="flex" flex={1} justifyContent="space-between">
+        <Stack gap={1}>
+          <Typography fontSize={14} variant="body4">
+            {t('explorer.curate.requester')}
+          </Typography>
+          <Address address={badge?.account.id} isUserAddress />
+        </Stack>
+        <Box alignItems="flex-end" display="flex">
+          <Typography fontSize={14} sx={{ textDecoration: 'underline !important' }} variant="body4">
+            <a
+              href={badgeKlerosMetadata.data?.requestBadgeEvidenceRawUrl}
+              rel="noreferrer"
+              target="_blank"
+            >
+              {t('explorer.curate.viewEvidence')}
+            </a>
+          </Typography>
+        </Box>
+      </Box>
+
+      {/* Badge Evidence */}
+      <Stack gap={2}>
+        {badgeEvidence?.columns.map((column) => {
+          return (
+            <Stack key={column.label + column.description}>
+              <SafeSuspense>
+                <DisplayEvidenceField
+                  columnItem={column}
+                  value={getEvidenceValue(
+                    badgeEvidence?.values,
+                    badgeEvidence?.columns,
+                    column.label,
+                    column.type,
+                  )}
+                />
+              </SafeSuspense>
+            </Stack>
+          )
+        })}
+      </Stack>
+
+      {/* Listing Criteria info */}
+      <Stack gap={1} position="relative">
+        <Typography fontSize={14} variant="body3">
+          {t('explorer.curate.listingCriteria')}
+        </Typography>
+        <SafeSuspense>
+          <ListingCriteriaPreview badgeModelId={badge?.badgeModel.id} />
+        </SafeSuspense>
+        <Divider color={colors.white} />
+      </Stack>
+
+      <Box display="flex" flex="1" justifyContent="space-between">
+        <ButtonV2
+          backgroundColor={colors.transparent}
+          fontColor={colors.white}
+          onClick={() => challenge(badge?.id)}
+          sx={{ ml: 'auto' }}
+          variant="outlined"
+        >
+          {t('profile.badgesIAmReviewing.addEvidence')}
+        </ButtonV2>
+      </Box>
+    </Stack>
+  )
+}
--- src/pagePartials/profile/reviewing/BadgesIAmReviewingSection.tsx
+++ src/pagePartials/profile/reviewing/BadgesIAmReviewingSection.tsx
@@ -1,43 +1,63 @@
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { RefObject, createRef, useState } from 'react'
 
-import { Box, Stack } from '@mui/material'
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined'
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined'
+import { Box, IconButton, Stack, Typography } from '@mui/material'
 import { colors } from '@thebadge/ui-library'
 import { useTranslation } from 'next-export-i18n'
 
 import { NoResultsAnimated } from '@/src/components/assets/animated/NoResults'
+import {
+  MiniBadgePreviewContainer,
+  MiniBadgePreviewLoading,
+} from '@/src/components/common/MiniBadgePreviewContainer'
 import FilteredList, { ListFilter } from '@/src/components/helpers/FilteredList'
+import InViewPort from '@/src/components/helpers/InViewPort'
+import SafeSuspense from '@/src/components/helpers/SafeSuspense'
 import { nowInSeconds } from '@/src/constants/helpers'
 import useSubgraph from '@/src/hooks/subgraph/useSubgraph'
+import useListItemNavigation from '@/src/hooks/useListItemNavigation'
 import MiniBadgeModelPreview from '@/src/pagePartials/badge/MiniBadgeModelPreview'
+import BadgeReviewingInfoPreview from '@/src/pagePartials/profile/reviewing/BadgeEvidenceInfoPreview'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
+import { Badge } from '@/types/generated/subgraph'
 
 const now = nowInSeconds()
+const filters: Array<ListFilter> = [
+  {
+    title: 'In Review',
+    color: 'green',
+    fixed: true,
+    defaultSelected: true,
+  },
+  {
+    title: 'Challenged',
+    color: 'pink',
+    fixed: true,
+    defaultSelected: true,
+  },
+]
 
 export default function BadgesIAmReviewingSection() {
   const { t } = useTranslation()
-  const filters: Array<ListFilter> = [
-    {
-      title: 'In Review',
-      color: 'green',
-      fixed: true,
-      defaultSelected: true,
-    },
-    {
-      title: 'Challenged',
-      color: 'pink',
-      fixed: true,
-      defaultSelected: true,
-    },
-  ]
-
-  const [items, setItems] = useState<React.ReactNode[]>([])
-  const [loading, setLoading] = useState<boolean>(false)
   const { address } = useWeb3Connection()
   const router = useRouter()
   const gql = useSubgraph()
 
-  if (!address) return null
+  const [badgesIamReviewing, setItems] = useState<Badge[]>([])
+  const [selectedBadge, setSelectedBadge] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const badgesElementRefs: RefObject<HTMLLIElement>[] = badgesIamReviewing.map(() =>
+    createRef<HTMLLIElement>(),
+  )
+  const { selectNext, selectPrevious } = useListItemNavigation(
+    setSelectedBadge,
+    badgesElementRefs,
+    selectedBadge,
+    badgesIamReviewing.length,
+  )
 
   const search = async (
     selectedFilters: Array<ListFilter>,
@@ -51,25 +71,46 @@ export default function BadgesIAmReviewingSection() {
     })
     const badgesIamReviewing = badgesMetadataIamReviewing.badgeKlerosMetaDatas.map(
       (metadata) => metadata.badge,
-    )
-    const badgesLayouts = badgesIamReviewing.map((badge) => {
-      const badgeModel = badge.badgeModel
-      return (
-        <Box key={badge.id}>
-          <MiniBadgeModelPreview
-            highlightColor={colors.green}
-            metadata={badgeModel?.uri}
-            onClick={() => router.push(`/badge/preview/${badge.id}`)}
-          />
-        </Box>
-      )
-    })
+    ) as Badge[]
+    //const badgesLayouts = badgesIamReviewing.map((badge) => {
+    //  const badgeModel = badge.badgeModel
+    //  return (
+    //    <Box key={badge.id}>
+    //      <MiniBadgeModelPreview
+    //        highlightColor={colors.green}
+    //        metadata={badgeModel?.uri}
+    //        onClick={() => router.push(`/badge/preview/${badge.id}`)}
+    //      />
+    //    </Box>
+    //  )
+    //})
 
-    setTimeout(() => {
-      setItems(badgesLayouts)
-      setLoading(false)
-    }, 2000)
+    setItems(badgesIamReviewing)
+    setLoading(false)
   }
+
+  function renderSelectedBadgePreview() {
+    if (!badgesIamReviewing[selectedBadge]) return null
+    return (
+      <SafeSuspense>
+        <Box display="flex" justifyContent="space-between">
+          <Typography color={colors.green} mb={4} variant="dAppHeadline2">
+            {t('explorer.preview.title')}
+          </Typography>
+          <Box>
+            <IconButton onClick={selectPrevious}>
+              <ArrowBackIosOutlinedIcon color="green" />
+            </IconButton>
+            <IconButton onClick={selectNext}>
+              <ArrowForwardIosOutlinedIcon color="green" />
+            </IconButton>
+          </Box>
+        </Box>
+        <BadgeReviewingInfoPreview badge={badgesIamReviewing[selectedBadge]} />
+      </SafeSuspense>
+    )
+  }
+  if (!address) return null
 
   return (
     <>
@@ -78,13 +119,34 @@ export default function BadgesIAmReviewingSection() {
         filters={filters}
         loading={loading}
         loadingColor={'green'}
+        preview={renderSelectedBadgePreview()}
         search={search}
         showTextSearch={false}
         title={t('profile.badgesIAmReviewing.title')}
         titleColor={colors.green}
       >
-        {items.length > 0 ? (
-          items
+        {badgesIamReviewing.length > 0 ? (
+          badgesIamReviewing.map((badge, i) => {
+            const isSelected = badge.id === badgesIamReviewing[selectedBadge]?.id
+            return (
+              <InViewPort key={badge.id} minHeight={300} minWidth={180}>
+                <SafeSuspense fallback={<MiniBadgePreviewLoading />}>
+                  <MiniBadgePreviewContainer
+                    highlightColor={colors.green}
+                    onClick={() => setSelectedBadge(i)}
+                    ref={badgesElementRefs[i]}
+                    selected={isSelected}
+                  >
+                    <MiniBadgeModelPreview
+                      disableAnimations
+                      highlightColor={colors.green}
+                      metadata={badge.badgeModel?.uri}
+                    />
+                  </MiniBadgePreviewContainer>
+                </SafeSuspense>
+              </InViewPort>
+            )
+          })
         ) : (
           <Stack>
             <NoResultsAnimated errorText={t('profile.badgesIAmReviewing.noResults')} />
