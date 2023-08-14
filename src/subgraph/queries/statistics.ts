9b5d41e8ccbdf46704edebbc96768fc9a6b2f3fb
Federico Madoery <fede.madoery@hotmail.com>
2023-08-14 17:56:18 -0300

    Fix ProtocolStatistics.tsx with new contracts

--- src/subgraph/queries/statistics.ts
+++ src/subgraph/queries/statistics.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export const PROTOCOL_STATISTICS = gql`
-  query protocolStatistic {
-    protocolStatistic(id: "0x641ddcede396fa1c2b1af323523508d2f8fd6825") {
+  query protocolStatistics {
+    protocolStatistics {
       badgeCreatorsAmount
       badgeCuratorsAmount
       badgeModelsCreatedAmount
