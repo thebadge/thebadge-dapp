d2248a0b9195e09a0b95dfae9853298bd316f228
Federico Madoery <fede.madoery@hotmail.com>
2023-08-22 07:52:56 -0300

    Improve mint cost component to show the total cost (#192)

--- src/pagePartials/badge/mint/steps/preview/MintCost.tsx
+++ src/pagePartials/badge/mint/steps/preview/MintCost.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined'
-import { Box, Divider, Stack, Typography } from '@mui/material'
+import { Box, Divider, Stack, Tooltip, Typography, styled } from '@mui/material'
 import { colors } from '@thebadge/ui-library'
 import { useTranslation } from 'next-export-i18n'
 
@@ -21,6 +21,21 @@ type Props = {
     totalMintCost: string
   }
 }
+
+const CostContainer = styled(Stack)(({ theme }) => ({
+  flex: 1,
+  gap: theme.spacing(1),
+  borderBottom: '1px solid white',
+  margin: 'auto',
+}))
+
+const ValueContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flex: 1,
+  justifyContent: 'space-between',
+  marginBottom: theme.spacing(1),
+}))
+
 export default function MintCost({ costs }: Props) {
   const { t } = useTranslation()
   const { appChainId } = useWeb3Connection()
@@ -59,55 +74,65 @@ export default function MintCost({ costs }: Props) {
         gap: 2,
       }}
     >
-      <Typography fontWeight={800} variant="dAppTitle3">
-        {t('badge.model.mint.depositRequired')}
-      </Typography>
       <Box display="flex" flex={1} gap={4}>
-        <Stack flex={1} gap={1} sx={{ borderBottom: '1px solid white', margin: 'auto' }}>
+        <CostContainer>
           <Typography variant="dAppTitle2">{t('badge.model.mint.depositCost')}</Typography>
-          <Typography variant="body2">
-            {costs.klerosCost} {networkConfig.token}
-          </Typography>
-        </Stack>
-        <Box display="flex" flex={1} gap={2}>
-          <ReportProblemOutlinedIcon sx={{ m: 'auto' }} />
-          <MarkdownTypography
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              fontWeight: 300,
-              '& a': { textDecoration: 'underline !important' },
-            }}
-            variant="subtitle2"
-          >
-            {t('badge.model.mint.depositDisclaimer', {
-              docsUrl: DOCS_URL + '/thebadge-documentation/protocol-mechanics/challenge',
-              challengePeriodDuration,
-              timeUnit: isTestnet ? 'minutes' : 'days',
-            })}
-          </MarkdownTypography>
-        </Box>
+          <ValueContainer>
+            <Typography variant="body2">
+              {costs.klerosCost} {networkConfig.token}
+            </Typography>
+            <Tooltip
+              arrow
+              title={
+                <MarkdownTypography
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    fontWeight: 300,
+                    '& a': { textDecoration: 'underline !important' },
+                  }}
+                  variant="subtitle2"
+                >
+                  {t('badge.model.mint.depositDisclaimer', {
+                    docsUrl: DOCS_URL + '/thebadge-documentation/protocol-mechanics/challenge',
+                    challengePeriodDuration,
+                    timeUnit: isTestnet ? 'minutes' : 'days',
+                  })}
+                </MarkdownTypography>
+              }
+            >
+              <ReportProblemOutlinedIcon />
+            </Tooltip>
+          </ValueContainer>
+        </CostContainer>
+        <CostContainer>
+          <Typography variant="dAppTitle2">{t('badge.model.mint.mintCost')}</Typography>
+          <ValueContainer>
+            <Typography sx={{ display: 'flex' }} variant="body2">
+              {costs.mintCost} {networkConfig.token}
+            </Typography>
+          </ValueContainer>
+        </CostContainer>
       </Box>
-      <Divider />
-      <Stack
-        flex={1}
-        gap={1}
-        sx={{
-          borderBottom: '1px solid white',
-          justifyContent: 'flex-end',
-          width: 'fit-content',
-          minWidth: '50%',
-        }}
-      >
-        <Typography variant="dAppTitle2">{t('badge.model.mint.mintCost')}</Typography>
-        <Box display="flex" justifyContent="space-between">
-          <Typography sx={{ display: 'flex' }} variant="body2">
-            {costs.mintCost} {networkConfig.token}
-          </Typography>
-        </Box>
-      </Stack>
 
-      <Typography textAlign="center" variant="subtitle2">
+      <Divider />
+
+      <CostContainer width="50%">
+        <Typography color={colors.green} variant="dAppTitle2">
+          {t('badge.model.mint.totalCost')}
+        </Typography>
+        <ValueContainer>
+          <Typography
+            color={colors.green}
+            sx={{ display: 'flex', fontWeight: 'bold' }}
+            variant="body2"
+          >
+            {costs.totalMintCost} {networkConfig.token}
+          </Typography>
+        </ValueContainer>
+      </CostContainer>
+
+      <Typography mt={2} textAlign="center" variant="subtitle2">
         {t('badge.model.mint.makeSure')}
         <a
           href={badgeCriteria as string}
