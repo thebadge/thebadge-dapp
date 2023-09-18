ff406a3982be8244132660191bb318f8b4012a39
apane <agustin.pane@gmail.com>
2023-09-18 19:48:50 +0200

    Implements MintThirdPartyBadgeModel

--- pages/badge/mint/[controllerType]/[modelId]/mintThirdParty.tsx
+++ pages/badge/mint/[controllerType]/[modelId]/mintThirdParty.tsx
@@ -3,42 +3,32 @@ import * as React from 'react'
 import { useEffect } from 'react'
 
 import { withPageGenericSuspense } from '@/src/components/helpers/SafeSuspense'
-import useControllerTypeParam from '@/src/hooks/nextjs/useControllerTypeParam'
+import { ZERO_ADDRESS } from '@/src/constants/bigNumber'
 import useModelIdParam from '@/src/hooks/nextjs/useModelIdParam'
 import useBadgeModel from '@/src/hooks/subgraph/useBadgeModel'
 import useMintValue from '@/src/hooks/theBadge/useMintValue'
 import { useContractInstance } from '@/src/hooks/useContractInstance'
 import useTransaction, { TransactionStates } from '@/src/hooks/useTransaction'
-import MintWithSteps from '@/src/pagePartials/badge/mint/MintWithSteps'
-import { MintBadgeSchemaType } from '@/src/pagePartials/badge/mint/schema/MintBadgeSchema'
+import MintThirdPartyWithSteps from '@/src/pagePartials/badge/mint/MintThirdPartyWithSteps'
+import { MintThirdPartySchemaType } from '@/src/pagePartials/badge/mint/schema/MintThirdPartySchema'
 import { cleanMintFormValues } from '@/src/pagePartials/badge/mint/utils'
 import { PreventActionIfBadgeTypePaused } from '@/src/pagePartials/errors/preventActionIfPaused'
 import { RequiredNotHaveBadge } from '@/src/pagePartials/errors/requiredNotHaveBadge'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
+import { encodeIpfsBadgeMetadata } from '@/src/utils/badges/createBadgeModelHelpers'
 import { BadgeModelMetadata } from '@/types/badges/BadgeMetadata'
 import { TheBadge__factory } from '@/types/generated/typechain'
 import { NextPageWithLayout } from '@/types/next'
-import { SupportedRelayMethods } from '@/types/relayedTx'
-import { encodeIpfsEvidence } from '@/src/utils/badges/createBadgeModelHelpers'
 
 const MintThirdPartyBadgeModel: NextPageWithLayout = () => {
-  const { address, appChainId, appPubKey, isSocialWallet, userSocialInfo, web3Provider } =
-    useWeb3Connection()
+  const { appPubKey, isSocialWallet, userSocialInfo } = useWeb3Connection()
   const theBadge = useContractInstance(TheBadge__factory, 'TheBadge')
-  const { resetTxState, sendRelayTx, sendTx, state } = useTransaction()
+  const { resetTxState, sendTx, state } = useTransaction()
   const router = useRouter()
   const badgeModelId = useModelIdParam()
-  const controllerType = useControllerTypeParam()
-  console.log('test1')
-  const test = encodeIpfsEvidence('0x0dc0dfD22C6Beab74672EADE5F9Be5234AAa43cC')
-  console.log('Test!', test)
 
   if (!badgeModelId) {
     throw `No modelId provided us URL query param`
-  }
-
-  if (!controllerType) {
-    throw `No controllerType provided us URL query param`
   }
 
   useEffect(() => {
@@ -49,7 +39,6 @@ const MintThirdPartyBadgeModel: NextPageWithLayout = () => {
   }, [router, state])
 
   const badgeModel = useBadgeModel(badgeModelId)
-  console.log('MintThirdPartyBadgeModel', badgeModel.data)
 
   if (badgeModel.error || !badgeModel.data) {
     throw `There was an error trying to fetch the metadata for the badge model`
@@ -60,56 +49,39 @@ const MintThirdPartyBadgeModel: NextPageWithLayout = () => {
     throw `There was not possible to get the value to mint a badge for the badge model: ${badgeModelId}`
   }
 
-  async function onSubmit(data: MintBadgeSchemaType) {
+  async function onSubmit(data: MintThirdPartySchemaType) {
     try {
       // Start transaction to show the loading state when we create the files
       // and configs
       const transaction = await sendTx(async () => {
-        const { previewImage } = data
+        const { address, preferMintMethod, previewImage } = data
         // Use NextJs dynamic import to reduce the bundle size
-        const { createAndUploadBadgeMetadata } = await import('@/src/utils/badges/mintHelpers')
+        const { createAndUploadThirdPartyBadgeMetadata } = await import(
+          '@/src/utils/badges/mintHelpers'
+        )
 
-        const badgeMetadataIPFSHash = await createAndUploadBadgeMetadata(
+        const badgeMetadataIPFSHash = await createAndUploadThirdPartyBadgeMetadata(
           badgeModel.data?.badgeModelMetadata as BadgeModelMetadata,
-          address as string,
+          badgeModelId,
           { imageBase64File: previewImage },
         )
 
         // If social login relay tx
         if (isSocialWallet && address && userSocialInfo && appPubKey) {
-          const data = JSON.stringify({
-            badgeModelId,
-            address,
-            badgeMetadataIPFSHash,
-            overrides: {
-              value: mintValue,
-            },
-          })
-
-          const signature = await web3Provider?.getSigner().signMessage(data)
-
-          if (!signature) {
-            throw new Error('User rejected the signing of the message')
-          }
-
-          // Send to backend
-          await sendRelayTx({
-            data,
-            from: address,
-            chainId: appChainId.toString(),
-            method: SupportedRelayMethods.MINT,
-            signature,
-            userAccount: {
-              address,
-              userSocialInfo,
-            },
-            appPubKey,
-          })
+          // TODO Implement social login (or maybe create a hook that encapsulates this logic)
         }
+
         // If user is not social logged, just send the tx
-        return theBadge.mint(badgeModelId, address as string, badgeMetadataIPFSHash, '0x', {
-          value: mintValue,
-        })
+        return theBadge.mint(
+          badgeModelId,
+          preferMintMethod === 'email' ? ZERO_ADDRESS : address,
+          badgeMetadataIPFSHash,
+          // TODO Check if this makes sense or not
+          encodeIpfsBadgeMetadata(badgeMetadataIPFSHash),
+          {
+            value: mintValue,
+          },
+        )
       })
       if (transaction) {
         await transaction.wait()
@@ -124,7 +96,7 @@ const MintThirdPartyBadgeModel: NextPageWithLayout = () => {
   return (
     <PreventActionIfBadgeTypePaused>
       <RequiredNotHaveBadge>
-        <MintWithSteps onSubmit={onSubmit} resetTxState={resetTxState} txState={state} />
+        <MintThirdPartyWithSteps onSubmit={onSubmit} resetTxState={resetTxState} txState={state} />
       </RequiredNotHaveBadge>
     </PreventActionIfBadgeTypePaused>
   )
