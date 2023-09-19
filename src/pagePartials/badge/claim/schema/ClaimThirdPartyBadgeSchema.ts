5f17c1cb41e38ae1c288759f8b70f724711e224d
apane <agustin.pane@gmail.com>
2023-09-19 15:32:25 +0200

    Updates ClaimThirdPartyBadgeSchema Adds StepClaimThirdPartyPreview Adds StepClaimThirdPartyHeader Adds StepClaimThirdParty

--- src/pagePartials/badge/claim/schema/ClaimThirdPartyBadgeSchema.ts
+++ src/pagePartials/badge/claim/schema/ClaimThirdPartyBadgeSchema.ts
@@ -4,6 +4,8 @@ import { AddressSchema } from '@/src/components/form/helpers/customSchemas'
 
 export const ClaimThirdPartyBadgeSchema = z.object({
   claimAddress: AddressSchema,
+  // Badge Image on base64, generated from the badgePreview
+  previewImage: z.string(),
 })
 
 export type ClaimThirdPartyBadgeSchemaType = z.infer<typeof ClaimThirdPartyBadgeSchema>
---
+++ src/pagePartials/badge/claim/steps/StepsClaimThirdPartyPreview.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { useRef } from 'react'
+
+import { Box, Stack } from '@mui/material'
+import { useTranslation } from 'next-export-i18n'
+import { useFormContext } from 'react-hook-form'
+
+import useModelIdParam from '@/src/hooks/nextjs/useModelIdParam'
+import { ClaimThirdPartyBadgeSchemaType } from '@/src/pagePartials/badge/claim/schema/ClaimThirdPartyBadgeSchema'
+import { BadgePreviewGenerator } from '@/src/pagePartials/badge/preview/BadgePreviewGenerator'
+
+export const StepClaimThirdPartyPreview = () => {
+  const { t } = useTranslation()
+  const badgePreviewRef = useRef<HTMLDivElement>()
+  const { setValue } = useFormContext<ClaimThirdPartyBadgeSchemaType>()
+  const modelId = useModelIdParam()
+
+  return (
+    <Stack alignItems={'center'} gap={3} margin={1}>
+      <Box ref={badgePreviewRef}>
+        <BadgePreviewGenerator
+          badgePreviewRef={badgePreviewRef}
+          modelId={modelId}
+          setValue={setValue}
+          title={t('badge.model.claim.thirdParty.preview.title')}
+        />
+      </Box>
+    </Stack>
+  )
+}
