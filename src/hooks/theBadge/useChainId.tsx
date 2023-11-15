6196b2cf04d377d9e33260450c75dda14fa9429b
Federico Madoery <fede.madoery@hotmail.com>
2023-11-15 19:00:06 -0300

    Maybe useChainId is returning undefined on early access

--- src/hooks/theBadge/useChainId.tsx
+++ src/hooks/theBadge/useChainId.tsx
@@ -1,7 +1,9 @@
 import { useWeb3ModalState } from '@web3modal/wagmi/react'
 
+import { ChainsValues } from '@/types/chains'
+
 export default function useChainId() {
   const { selectedNetworkId } = useWeb3ModalState()
 
-  return selectedNetworkId
+  return (selectedNetworkId || 11155111) as ChainsValues
 }
