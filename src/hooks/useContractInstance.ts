3890f08b4a8169e061c8c9fe5372f30d1d019c77
Federico Madoery <fede.madoery@hotmail.com>
2023-11-16 12:23:49 -0300

    Update code to get it work on walletClientToSigner

--- src/hooks/useContractInstance.ts
+++ src/hooks/useContractInstance.ts
@@ -14,10 +14,10 @@ export const useContractInstance = <F extends AppFactories, RT extends ReturnTyp
   address?: string,
 ) => {
   const { appChainId } = useWeb3Connection()
-  const signer = useEthersSigner()
+  const signer = useEthersSigner({ chainId: appChainId })
   const _address = address ? address : contracts[contractKey]['address'][appChainId]
   if (!_address) throw `Address for ${contractKey} and ${appChainId} is null`
-  if (!signer) throw 'There is not signer to execute a tx.'
+  if (!signer) throw 'There is no signer to execute the transaction.'
 
   return contractFactory.connect(_address, signer) as RT
 }
