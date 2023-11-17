40bff1ce3777d9002cad9b7e002df0c9e601e62e
Agustin Pane <agustin.pane@gmail.com>
2023-11-17 23:14:37 +0300

    Fixes generateBadgePreviewUrl

--- src/pagePartials/errors/displays/AlreadyOwnThisBadgeError.tsx
+++ src/pagePartials/errors/displays/AlreadyOwnThisBadgeError.tsx
@@ -8,6 +8,7 @@ import { ButtonV2, colors, gradients } from '@thebadge/ui-library'
 import { useTranslation } from 'next-export-i18n'
 
 import useModelIdParam from '@/src/hooks/nextjs/useModelIdParam'
+import useBadgeModel from '@/src/hooks/subgraph/useBadgeModel'
 import { useBadgeOwnershipData } from '@/src/hooks/subgraph/useIsBadgeOwner'
 import { useColorMode } from '@/src/providers/themeProvider'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
@@ -39,13 +40,18 @@ export default function AlreadyOwnThisBadgeError({ onClose }: { onClose: VoidFun
   const { t } = useTranslation()
   const { mode } = useColorMode()
   const router = useRouter()
-  const { address } = useWeb3Connection()
+  const { address, appChainId } = useWeb3Connection()
   const badgeModelId = useModelIdParam()
-
+  const badgeModelQuery = useBadgeModel(badgeModelId)
+  const badgeModel = badgeModelQuery?.data?.badgeModel
   const ownedBadges = useBadgeOwnershipData(badgeModelId, address as string)
 
   if (!ownedBadges || !ownedBadges.length) {
     // This case it would never happen, but it's the safest way to proceed
+    onClose()
+    return null
+  }
+  if (!badgeModel) {
     onClose()
     return null
   }
@@ -72,7 +78,12 @@ export default function AlreadyOwnThisBadgeError({ onClose }: { onClose: VoidFun
         <ButtonV2
           backgroundColor={colors.transparent}
           fontColor={mode === 'light' ? colors.blackText : colors.white}
-          onClick={() => router.push(generateBadgePreviewUrl(ownedBadges[0].id))}
+          onClick={() => {
+            const selectedBadge = ownedBadges[0]
+            return router.push(
+              generateBadgePreviewUrl(selectedBadge.id, badgeModel.contractAddress, appChainId),
+            )
+          }}
           sx={{
             borderRadius: '10px',
             fontSize: '14px !important',
