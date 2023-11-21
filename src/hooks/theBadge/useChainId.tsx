1cebb8fc3fa5ded749666d9393e900819b1d2093
Federico Madoery <fede.madoery@hotmail.com>
2023-11-21 19:54:58 -0300

    Add dynamic import

--- src/hooks/theBadge/useChainId.tsx
+++ src/hooks/theBadge/useChainId.tsx
@@ -1,4 +1,4 @@
-import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
+const { useWeb3Connection } = await import('@/src/providers/web3ConnectionProvider')
 
 export default function useChainId() {
   const { appChainId } = useWeb3Connection()
