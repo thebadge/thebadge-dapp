4922042438cbbff327badf2c9f37c199bfb070bb
apane <agustin.pane@gmail.com>
2023-11-28 17:38:20 +0100

    Adds useSendClaimEmail hook

---
+++ src/hooks/theBadge/useSendClaimEmail.ts
@@ -0,0 +1,55 @@
+import { useSignMessage } from 'wagmi'
+
+import { notify } from '@/src/components/toast/Toast'
+import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
+import { sendEmailClaim } from '@/src/utils/relayTx'
+import { EmailClaimTx } from '@/types/relayedTx'
+import { ToastStates } from '@/types/toast'
+import { BackendResponse } from '@/types/utils'
+
+type Response = Promise<BackendResponse<{ txHash: string | null }>>
+
+export default function useSendClaimEmail(): {
+  submitSendClaimEmail: (props: EmailClaimTx) => Response
+} {
+  const { signMessageAsync } = useSignMessage()
+  const { address } = useWeb3Connection()
+
+  const submitSendClaimEmail = async (props: EmailClaimTx): Response => {
+    const { badgeModelId, emailClaimer, mintTxHash, networkId } = props
+    const signedMessage = `I accept the terms & conditions and I verify to be the owner of the address: ${address}`
+    const signature = await signMessageAsync({
+      message: signedMessage,
+    })
+
+    const result = await sendEmailClaim({
+      networkId,
+      mintTxHash,
+      badgeModelId,
+      emailClaimer,
+      signature,
+      signedMessage,
+      ownerAddress: address as string,
+    })
+
+    if (result.error) {
+      notify({
+        id: mintTxHash,
+        type: ToastStates.infoFailed,
+        message: result.message,
+        position: 'bottom-right',
+      })
+    } else {
+      notify({
+        id: mintTxHash,
+        type: ToastStates.info,
+        message: result.message,
+        position: 'bottom-right',
+      })
+    }
+
+    return result
+  }
+
+  return { submitSendClaimEmail }
+}
