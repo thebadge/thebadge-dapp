85cda62f35b679a33f7800e8e6126a4c6f7b24c2
apane <agustin.pane@gmail.com>
2023-11-30 13:41:11 +0100

    Fix third party profile routing

--- pages/badgeModel/create/[controllerType]/index.tsx
+++ pages/badgeModel/create/[controllerType]/index.tsx
@@ -1,31 +1,18 @@
-import { useRouter } from 'next/router'
 import * as React from 'react'
-import { useEffect } from 'react'
 
 import { withPageGenericSuspense } from '@/src/components/helpers/SafeSuspense'
 import useControllerTypeParam from '@/src/hooks/nextjs/useControllerTypeParam'
-import useTransaction, { TransactionStates } from '@/src/hooks/useTransaction'
 import CreateCommunityBadgeModel from '@/src/pagePartials/badge/model/CreateCommunityBadgeModel'
 import CreateThirdPartyBadgeModel from '@/src/pagePartials/badge/model/CreateThirdPartyBadgeModel'
-import { generateProfileUrl } from '@/src/utils/navigation/generateUrl'
 import { BadgeModelControllerType } from '@/types/badges/BadgeModel'
 import { NextPageWithLayout } from '@/types/next'
 
 const CreateBadgeModel: NextPageWithLayout = () => {
-  const { state } = useTransaction()
-  const router = useRouter()
   const controllerType = useControllerTypeParam()
 
   if (!controllerType) {
     throw `No controllerType provided as URL query param`
   }
-
-  useEffect(() => {
-    // Redirect to the profile
-    if (state === TransactionStates.success) {
-      router.push(generateProfileUrl())
-    }
-  }, [router, state])
 
   switch (controllerType.toLowerCase()) {
     case BadgeModelControllerType.Community.toLowerCase(): {
