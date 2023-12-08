2cc75961a229762a48a26663b00463698787af4e
apane <agustin.pane@gmail.com>
2023-12-08 14:44:06 +0100

    Fixes cache management and adds etags to the requests

--- src/hooks/subgraph/utils.ts
+++ src/hooks/subgraph/utils.ts
@@ -11,28 +11,47 @@ import { BackendResponse } from '@/types/utils'
  * @template X - Additional type parameter that can be optionally provided to extend the backend response type.
  * @param {string} hash - The IPFS hash representing the content to retrieve.
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export async function getFromIPFS<T, X = {}>(hash?: string) {
+export async function getFromIPFS<T, X = {}>(hash?: string): Promise<any> {
   if (!hash) return
   const cleanedHash = cleanHash(hash as string)
   if (!cleanedHash) {
     console.warn('Invalid hash provided on getFromIPFS', { hash })
     return
   }
+  const itemCacheKey = cleanedHash
+  const etagCacheKey = `etag-${cleanedHash}`
+  const url = `${BACKEND_URL}/api/ipfs/${cleanedHash}`
 
-  const cachedResponse = getCacheResponse<BackendResponse<{ content: T } & X>>(
-    `${BACKEND_URL}/api/ipfs/${cleanedHash}`,
-  )
-  if (cachedResponse) return cachedResponse
+  const cachedResponse = getCacheResponse<BackendResponse<{ content: T } & X>>(itemCacheKey)
+  if (cachedResponse) {
+    return cachedResponse
+  }
 
-  const backendResponse = await axios.get<BackendResponse<{ content: T } & X>>(
-    `${BACKEND_URL}/api/ipfs/${cleanedHash}`,
-  )
-  if (!backendResponse.data) return backendResponse
+  const etag = getCacheResponse(etagCacheKey)
+  const headers = etag ? { 'If-None-Match': etag } : {}
+  try {
+    const backendResponse = await axios.get<BackendResponse<{ content: T } & X>>(url, {
+      headers: headers as Record<string, string>,
+    })
+    if (!backendResponse.data) return backendResponse
 
-  // If the response has something on it, we store it on cache
-  saveResponseOnCache(`${BACKEND_URL}/api/ipfs/${cleanedHash}`, backendResponse)
-  return backendResponse
+    // If the response has something on it, we store it on cache
+    saveResponseOnCache(itemCacheKey, backendResponse)
+    saveResponseOnCache(etagCacheKey, backendResponse.headers.etag)
+    return backendResponse
+  } catch (error) {
+    // Handle 304 Not Modified response
+    // @ts-ignore
+    if (error.response && error.response.status === 304) {
+      // Handle the case where the resource has not been modified
+      console.log('Resource not modified')
+      return getCacheResponse(itemCacheKey)
+    }
+
+    // Handle other errors
+    console.error('Error fetching from IPFS', error)
+    throw error
+  }
 }
 
 /**
