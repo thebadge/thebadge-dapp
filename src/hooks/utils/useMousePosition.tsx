61298779227210dd4c846881f65c86a6a5eecce1
Federico Madoery <fede.madoery@hotmail.com>
2023-12-11 09:24:57 -0300

    Fix build error

--- src/hooks/utils/useMousePosition.tsx
+++ src/hooks/utils/useMousePosition.tsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react'
 
 export type MousePosition = {
-  x: number | null;
-  y: number | null;
-};
+  x: number | null
+  y: number | null
+}
 
 const useMousePosition = ({ lockPosition = false }: { lockPosition: boolean }) => {
   const [mousePosition, setMousePosition] = useState<MousePosition>({
     x: null,
-    y: null
-  });
+    y: null,
+  })
   useEffect(() => {
-    if (lockPosition) return;
+    if (lockPosition) return
     const updateMousePosition = (ev: MouseEvent) => {
-      setMousePosition({ x: ev.clientX, y: ev.clientY });
-    };
-    window.addEventListener("mousemove", updateMousePosition);
+      setMousePosition({ x: ev.clientX, y: ev.clientY })
+    }
+    window.addEventListener('mousemove', updateMousePosition)
     return () => {
-      window.removeEventListener("mousemove", updateMousePosition);
-    };
-  }, [lockPosition]);
-  return mousePosition;
-};
-export default useMousePosition;
+      window.removeEventListener('mousemove', updateMousePosition)
+    }
+  }, [lockPosition])
+  return mousePosition
+}
+export default useMousePosition
