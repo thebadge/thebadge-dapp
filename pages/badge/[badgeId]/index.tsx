d358349f0cf5cc10c14a973ccfb4140c2086fa59
Federico Madoery <fede.madoery@hotmail.com>
2023-12-20 16:28:04 -0300

    Remove all to se if it works

--- pages/badge/[badgeId]/index.tsx
+++ pages/badge/[badgeId]/index.tsx
@@ -1,176 +1,22 @@
-import { useRouter } from 'next/navigation'
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next/types'
 import { ReactElement } from 'react'
 import * as React from 'react'
 
-import { Box, Stack } from '@mui/material'
-import { ButtonV2, colors } from '@thebadge/ui-library'
-import { useTranslation } from 'next-export-i18n'
-
-import SafeSuspense from '@/src/components/helpers/SafeSuspense'
 import { ssrGetContentFromIPFS } from '@/src/hooks/subgraph/utils'
-import useBadgeClaim from '@/src/hooks/theBadge/useBadgeClaim'
-import useBadgeHelpers, { ReviewBadge } from '@/src/hooks/theBadge/useBadgeHelpers'
-import { useSizeSM } from '@/src/hooks/useSize'
-import BadgeOwnedPreview from '@/src/pagePartials/badge/preview/BadgeOwnedPreview'
-import DiplomaOwnedPreview from '@/src/pagePartials/badge/preview/DiplomaOwnedPreview'
-import ChallengedStatusLogo from '@/src/pagePartials/badge/preview/addons/ChallengedStatusLogo'
 import { CustomPageHead } from '@/src/pagePartials/index/CustomPageHead'
-import { useCurateProvider } from '@/src/providers/curateProvider'
-import { useColorMode } from '@/src/providers/themeProvider'
 import { gqlQuery } from '@/src/subgraph/subgraph'
 import devEndpoints from '@/src/subgraph/subgraph-endpoints-dev.json'
 import endpoints from '@/src/subgraph/subgraph-endpoints.json'
-import { generateMintUrl } from '@/src/utils/navigation/generateUrl'
 import { isTestnet } from '@/src/utils/network'
 import { parsePrefixedAddress } from '@/src/utils/prefixedAddress'
-import {
-  BadgeMetadata,
-  BadgeModelMetadata,
-  DiplomaNFTAttributesType,
-} from '@/types/badges/BadgeMetadata'
-import { BadgeModelControllerType, BadgeModelTemplate } from '@/types/badges/BadgeModel'
-import { BadgeByIdDocument, BadgeStatus } from '@/types/generated/subgraph'
-import { BackendFileResponse, WCAddress } from '@/types/utils'
-
-const { useWeb3Connection } = await import('@/src/providers/web3ConnectionProvider')
+import { BadgeMetadata, BadgeModelMetadata } from '@/types/badges/BadgeMetadata'
+import { BadgeByIdDocument } from '@/types/generated/subgraph'
+import { BackendFileResponse } from '@/types/utils'
 
 const ViewBadge = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(props)
-  const { t } = useTranslation()
-  const { address } = useWeb3Connection()
-  const { curate } = useCurateProvider()
-  const { getBadgeReviewStatus } = useBadgeHelpers()
-  const router = useRouter()
-  const { mode } = useColorMode()
-  const handleClaimBadge = useBadgeClaim()
-  const isMobile = useSizeSM()
-
-  const badgeId = props.badge.id
-  const badge = props.badge
-
-  const badgeModelId = badge.badgeModel.id
-  const ownerAddress = badge.account.id as WCAddress
-
-  const { reviewTimeFinished: badgeReviewTimeFinished, status: badgeStatus } = getBadgeReviewStatus(
-    badge as ReviewBadge,
-  )
-  const badgeModelMetadata = badge.badgeModel
-    .badgeModelMetadata as BadgeModelMetadata<BackendFileResponse>
-  const template = badgeModelMetadata?.attributes?.find(
-    (at) => at.trait_type === DiplomaNFTAttributesType.Template,
-  )?.value as BadgeModelTemplate
-
-  const isThirdPartyBadge = badge.badgeModel.controllerType === BadgeModelControllerType.ThirdParty
-
-  // Show mint button if this is not the own badge
-  const showMintButton = address !== ownerAddress && !isThirdPartyBadge
-
-  // Show curate button if this is not the own badge and its not already challenged */
-  const showCurateButton = address !== ownerAddress && !isThirdPartyBadge
-
-  // Show claim button if it is an own badge, it has status requested and the review time finished
-  const showClaimButton =
-    address === ownerAddress &&
-    badgeStatus === BadgeStatus.Requested &&
-    badgeReviewTimeFinished &&
-    !isThirdPartyBadge
-
   return (
     <>
       <CustomPageHead {...props.metaTags} />
-      <SafeSuspense>
-        <Box sx={{ position: 'relative' }}>
-          <Stack maxWidth={900} mx={'auto'}>
-            {badge?.status === BadgeStatus.Challenged && <ChallengedStatusLogo />}
-            {template === BadgeModelTemplate.Diploma ? (
-              <DiplomaOwnedPreview />
-            ) : (
-              <BadgeOwnedPreview />
-            )}
-            <Box display="flex" gap={8}>
-              {!isMobile && (
-                <Box
-                  alignItems="center"
-                  display="flex"
-                  flex="1"
-                  justifyContent="space-between"
-                  maxWidth={300}
-                >
-                  {showMintButton && (
-                    <ButtonV2
-                      backgroundColor={colors.transparent}
-                      disabled={address === ownerAddress}
-                      fontColor={mode === 'light' ? colors.blackText : colors.white}
-                      onClick={() =>
-                        router.push(generateMintUrl(badge?.badgeModel.controllerType, badgeModelId))
-                      }
-                      sx={{
-                        borderRadius: '10px',
-                        fontSize: '11px !important',
-                        padding: '0.5rem 1rem !important',
-                        height: 'fit-content !important',
-                        lineHeight: '14px',
-                        fontWeight: 700,
-                        boxShadow: 'none',
-                        textTransform: 'uppercase',
-                      }}
-                    >
-                      {t('badge.mintButton')}
-                    </ButtonV2>
-                  )}
-
-                  {showCurateButton && (
-                    <ButtonV2
-                      backgroundColor={colors.greenLogo}
-                      disabled={address === ownerAddress}
-                      fontColor={colors.blackText}
-                      onClick={() => curate(badgeId)}
-                      sx={{
-                        borderRadius: '10px',
-                        fontSize: '11px !important',
-                        padding: '0.5rem 1rem !important',
-                        height: 'fit-content !important',
-                        lineHeight: '14px',
-                        fontWeight: 700,
-                        boxShadow: 'none',
-                        textTransform: 'uppercase',
-                      }}
-                      variant="contained"
-                    >
-                      {t('badge.curateButton')}
-                    </ButtonV2>
-                  )}
-
-                  {showClaimButton && (
-                    <ButtonV2
-                      backgroundColor={colors.blue}
-                      disabled={!badgeReviewTimeFinished}
-                      fontColor={colors.white}
-                      onClick={() => handleClaimBadge(badge.id)}
-                      sx={{
-                        width: '100%',
-                        height: 'fit-content !important',
-                        marginTop: '1rem',
-                        padding: '0.5rem 1rem !important',
-                        borderRadius: '10px',
-                        fontSize: '15px !important',
-                        lineHeight: '15px',
-                        fontWeight: 700,
-                        boxShadow: 'none',
-                        textTransform: 'uppercase',
-                      }}
-                      variant="contained"
-                    >
-                      {t('badge.claimButton')}
-                    </ButtonV2>
-                  )}
-                </Box>
-              )}
-            </Box>
-          </Stack>
-        </Box>
-      </SafeSuspense>
     </>
   )
 }
