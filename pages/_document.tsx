d16612627e21fb55c585a0263a600bcd79248466
Federico Madoery <fede.madoery@hotmail.com>
2023-12-20 17:05:39 -0300

    Add metadata on _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,123 +5,170 @@ import { EmotionCache } from '@emotion/react'
 import createEmotionServer from '@emotion/server/create-instance'
 
 import { APP_URL } from '@/src/constants/common'
+import { ssrGetContentFromIPFS } from '@/src/hooks/subgraph/utils'
+import { gqlQuery } from '@/src/subgraph/subgraph'
+import devEndpoints from '@/src/subgraph/subgraph-endpoints-dev.json'
+import endpoints from '@/src/subgraph/subgraph-endpoints.json'
+import { isTestnet } from '@/src/utils/network'
+import { parsePrefixedAddress } from '@/src/utils/prefixedAddress'
+import { BadgeMetadata, BadgeModelMetadata } from '@/types/badges/BadgeMetadata'
+import { BadgeByIdDocument } from '@/types/generated/subgraph'
+import { BackendFileResponse } from '@/types/utils'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    // Resolution order
-    //
-    // On the server:
-    // 1. app.getInitialProps
-    // 2. page.getInitialProps
-    // 3. document.getInitialProps
-    // 4. app.render
-    // 5. page.render
-    // 6. document.render
-    //
-    // On the server with error:
-    // 1. document.getInitialProps
-    // 2. app.render
-    // 3. page.render
-    // 4. document.render
-    //
-    // On the client
-    // 1. app.getInitialProps
-    // 2. page.getInitialProps
-    // 3. app.render
-    // 4. page.render
+export default function MyDocument(props: any) {
+  const { metaTags } = props
 
-    const cache = createCache({
-      key: 'css',
-      prepend: true,
-    }) as EmotionCache
-    const { extractCriticalToChunks } = createEmotionServer(cache)
+  const title = metaTags?.title || 'The Badge dApp - Decentralized Certifications Platform'
+  const description =
+    metaTags?.description ||
+    'TheBadge is a decentralized certification platform that allows companies, institutions, and individuals to bring verified real-world certifications to the blockchain.'
+  const imageUrl = metaTags?.imageUrl || `${APP_URL}/shareable/the_badge_banner.webp`
 
-    const startTime = Date.now()
-    try {
-      /**
-       * Render the page as normal, but now that ApolloClient is initialized and the cache is full, each query will actually work.
-       */
-      const initialProps = await Document.getInitialProps(ctx)
+  const twitterHandle = '@thebadgexyz'
 
-      // This is important. It prevents emotion to render invalid HTML.
-      // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-      const emotionStyles = extractCriticalToChunks(initialProps.html)
-      const emotionStyleTags = emotionStyles.styles.map((style) => (
-        <style
-          dangerouslySetInnerHTML={{ __html: style.css }}
-          data-emotion={`${style.key} ${style.ids.join(' ')}`}
-          key={style.key}
-        />
-      ))
+  return (
+    <Html lang="en">
+      <Head>
+        <meta content="website" property="og:type" />
+        <meta content={description} name="description" />
 
-      console.info(`Render Time: ${Date.now() - startTime} milliseconds.`)
+        <meta content={`${APP_URL}`} property="og:url" />
 
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {emotionStyleTags}
-            {initialProps.styles}
-          </>
-        ),
-      }
-    } catch (error) {
-      console.error(error)
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-      }
+        <meta content={imageUrl} property="og:image" />
+        <meta content="image/webp" property="og:image:type" />
+        <meta content="1404" property="og:image:width" />
+        <meta content="459" property="og:image:height" />
+
+        <meta content={description} property="og:description" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content={title} name="twitter:site" />
+        <meta content={twitterHandle} name="twitter:creator" />
+
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content={twitterHandle} name="twitter:site" />
+        <meta content={imageUrl} name="twitter:image" />
+
+        <link href="/favicon/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon/site.webmanifest" rel="manifest" />
+
+        <link color="#333" href="/favicon/safari-pinned-tab.svg" rel="mask-icon" />
+        <meta content="#333" name="msapplication-TileColor" />
+        <meta content="#333" name="theme-color" />
+
+        {/*!--link manifest.json --*/}
+        <link href="/manifest.json" rel="manifest" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <div id="modals" />
+      </body>
+    </Html>
+  )
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const cache = createCache({
+    key: 'css',
+    prepend: true,
+  }) as EmotionCache
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  const startTime = Date.now()
+  const badgeId = ctx.query?.badgeId
+  const contract = ctx.query?.contract as string
+  let metaTags = {}
+  // Fetch data for the preview
+  if (badgeId && contract) {
+    const { chainId, subgraphName } = parsePrefixedAddress(contract)
+
+    const subGraph = isTestnet
+      ? devEndpoints[chainId][subgraphName]
+      : endpoints[chainId][subgraphName]
+
+    // Fetch data from external API
+    const request = await gqlQuery(subGraph, {
+      query: BadgeByIdDocument,
+      variables: { id: badgeId },
+    })
+
+    const badge = request.badge
+    const badgeModel = badge?.badgeModel
+
+    const metadataRequest = await Promise.all([
+      ssrGetContentFromIPFS<BadgeMetadata<BackendFileResponse>>(badge?.uri),
+      ssrGetContentFromIPFS<BadgeModelMetadata<BackendFileResponse>>(badgeModel?.uri),
+    ])
+
+    const badgeMetadata = metadataRequest[0] ? metadataRequest[0].result?.content : null
+    const badgeModelMetadata = metadataRequest[1] ? metadataRequest[1].result?.content : null
+    metaTags = {
+      title: `The Badge dApp - ${badgeModelMetadata?.name} Certificate`,
+      description: `${badgeModelMetadata?.description} - Powered by TheBadge `,
+      imageUrl: badgeMetadata?.image.s3Url,
     }
   }
 
-  title = 'The Badge dApp - Decentralized Certifications Platform'
-  description =
-    'TheBadge is a decentralized certification platform that allows companies, institutions, and individuals to bring verified real-world certifications to the blockchain.'
+  try {
+    /**
+     * Render the page as normal, but now that ApolloClient is initialized and the cache is full, each query will actually work.
+     */
+    const initialProps = await Document.getInitialProps(ctx)
 
-  twitterHandle = '@thebadgexyz'
+    // This is important. It prevents emotion to render invalid HTML.
+    // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+    const emotionStyles = extractCriticalToChunks(initialProps.html)
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        dangerouslySetInnerHTML={{ __html: style.css }}
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+      />
+    ))
 
-  render = () => {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta content="website" property="og:type" />
-          <meta content={this.description} name="description" />
+    console.info(`Render Time: ${Date.now() - startTime} milliseconds.`)
 
-          <meta content={`${APP_URL}`} property="og:url" />
-
-          <meta content={`${APP_URL}/shareable/the_badge_banner.webp`} property="og:image" />
-          <meta content="image/webp" property="og:image:type" />
-          <meta content="1404" property="og:image:width" />
-          <meta content="459" property="og:image:height" />
-
-          <meta content={this.description} property="og:description" />
-          <meta content="summary_large_image" name="twitter:card" />
-          <meta content={this.title} name="twitter:site" />
-          <meta content={this.twitterHandle} name="twitter:creator" />
-
-          <meta content="summary_large_image" name="twitter:card" />
-          <meta content={this.twitterHandle} name="twitter:site" />
-          <meta content={`${APP_URL}/shareable/the_badge_banner.webp`} name="twitter:image" />
-
-          <link href="/favicon/favicon.svg" rel="icon" type="image/svg+xml" />
-          <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
-          <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-          <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-          <link href="/favicon/site.webmanifest" rel="manifest" />
-
-          <link color="#333" href="/favicon/safari-pinned-tab.svg" rel="mask-icon" />
-          <meta content="#333" name="msapplication-TileColor" />
-          <meta content="#333" name="theme-color" />
-
-          {/*!--link manifest.json --*/}
-          <link href="/manifest.json" rel="manifest" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <div id="modals" />
-        </body>
-      </Html>
-    )
+    return {
+      ...initialProps,
+      metaTags,
+      styles: (
+        <>
+          {emotionStyleTags}
+          {initialProps.styles}
+        </>
+      ),
+    }
+  } catch (error) {
+    console.error(error)
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      metaTags,
+    }
   }
 }
--- pages/badge/[badgeId]/index.tsx
+++ pages/badge/[badgeId]/index.tsx
@@ -1,79 +1,169 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next/types'
-import { ReactElement } from 'react'
+import { useRouter } from 'next/navigation'
 import * as React from 'react'
 
-import { ssrGetContentFromIPFS } from '@/src/hooks/subgraph/utils'
-import { CustomPageHead } from '@/src/pagePartials/index/CustomPageHead'
-import { gqlQuery } from '@/src/subgraph/subgraph'
-import devEndpoints from '@/src/subgraph/subgraph-endpoints-dev.json'
-import endpoints from '@/src/subgraph/subgraph-endpoints.json'
-import { isTestnet } from '@/src/utils/network'
-import { parsePrefixedAddress } from '@/src/utils/prefixedAddress'
-import { BadgeMetadata, BadgeModelMetadata } from '@/types/badges/BadgeMetadata'
-import { BadgeByIdDocument } from '@/types/generated/subgraph'
-import { BackendFileResponse } from '@/types/utils'
+import { Box, Stack } from '@mui/material'
+import { ButtonV2, colors } from '@thebadge/ui-library'
+import { useTranslation } from 'next-export-i18n'
 
-const ViewBadge = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+import SafeSuspense, { withPageGenericSuspense } from '@/src/components/helpers/SafeSuspense'
+import useBadgeIdParam from '@/src/hooks/nextjs/useBadgeIdParam'
+import useBadgeById from '@/src/hooks/subgraph/useBadgeById'
+import useIsThirdPartyBadge from '@/src/hooks/subgraph/useIsThirdPartyBadge'
+import useBadgeClaim from '@/src/hooks/theBadge/useBadgeClaim'
+import useBadgeHelpers, { ReviewBadge } from '@/src/hooks/theBadge/useBadgeHelpers'
+import useBadgeModelTemplate from '@/src/hooks/theBadge/useBadgeModelTemplate'
+import { useSizeSM } from '@/src/hooks/useSize'
+import BadgeOwnedPreview from '@/src/pagePartials/badge/preview/BadgeOwnedPreview'
+import BadgeOwnerPreview from '@/src/pagePartials/badge/preview/BadgeOwnerPreview'
+import BadgeStatusAndEvidence from '@/src/pagePartials/badge/preview/BadgeStatusAndEvidence'
+import DiplomaOwnedPreview from '@/src/pagePartials/badge/preview/DiplomaOwnedPreview'
+import ChallengedStatusLogo from '@/src/pagePartials/badge/preview/addons/ChallengedStatusLogo'
+import { useCurateProvider } from '@/src/providers/curateProvider'
+import { useColorMode } from '@/src/providers/themeProvider'
+import { generateMintUrl } from '@/src/utils/navigation/generateUrl'
+import { BadgeModelTemplate } from '@/types/badges/BadgeModel'
+import { BadgeStatus } from '@/types/generated/subgraph'
+import { NextPageWithLayout } from '@/types/next'
+import { WCAddress } from '@/types/utils'
+const { useWeb3Connection } = await import('@/src/providers/web3ConnectionProvider')
+
+const ViewBadge: NextPageWithLayout = () => {
+  const { t } = useTranslation()
+  const { address } = useWeb3Connection()
+  const { curate } = useCurateProvider()
+  const { getBadgeReviewStatus } = useBadgeHelpers()
+  const router = useRouter()
+  const { mode } = useColorMode()
+  const handleClaimBadge = useBadgeClaim()
+  const isMobile = useSizeSM()
+
+  const { badgeId, contract } = useBadgeIdParam()
+  if (!badgeId) {
+    throw `No badgeId provided us URL query param`
+  }
+
+  const badgeById = useBadgeById(badgeId, contract)
+  const badge = badgeById.data
+
+  if (!badge) {
+    throw 'There was not possible to get the needed data. Try again in some minutes.'
+  }
+  const badgeModelId = badge.badgeModel.id
+  const ownerAddress = badge.account.id as WCAddress
+
+  const { reviewTimeFinished: badgeReviewTimeFinished, status: badgeStatus } = getBadgeReviewStatus(
+    badge as ReviewBadge,
+  )
+  const template = useBadgeModelTemplate(badgeModelId, contract)
+  const isThirdPartyBadge = useIsThirdPartyBadge(badgeId, contract)
+
+  // Show mint button if this is not the own badge
+  const showMintButton = address !== ownerAddress && !isThirdPartyBadge
+
+  // Show curate button if this is not the own badge and its not already challenged */
+  const showCurateButton = address !== ownerAddress && !isThirdPartyBadge
+
+  // Show claim button if it is an own badge, it has status requested and the review time finished
+  const showClaimButton =
+    address === ownerAddress &&
+    badgeStatus === BadgeStatus.Requested &&
+    badgeReviewTimeFinished &&
+    !isThirdPartyBadge
+
   return (
-    <>
-      <CustomPageHead {...props.metaTags} />
-    </>
+    <Box sx={{ position: 'relative' }}>
+      <Stack maxWidth={900} mx={'auto'}>
+        {badge?.status === BadgeStatus.Challenged && <ChallengedStatusLogo />}
+        {template === BadgeModelTemplate.Diploma ? <DiplomaOwnedPreview /> : <BadgeOwnedPreview />}
+        <Box display="flex" gap={8}>
+          {!isMobile && (
+            <Box
+              alignItems="center"
+              display="flex"
+              flex="1"
+              justifyContent="space-between"
+              maxWidth={300}
+            >
+              {showMintButton && (
+                <ButtonV2
+                  backgroundColor={colors.transparent}
+                  disabled={address === ownerAddress}
+                  fontColor={mode === 'light' ? colors.blackText : colors.white}
+                  onClick={() =>
+                    router.push(generateMintUrl(badge?.badgeModel.controllerType, badgeModelId))
+                  }
+                  sx={{
+                    borderRadius: '10px',
+                    fontSize: '11px !important',
+                    padding: '0.5rem 1rem !important',
+                    height: 'fit-content !important',
+                    lineHeight: '14px',
+                    fontWeight: 700,
+                    boxShadow: 'none',
+                    textTransform: 'uppercase',
+                  }}
+                >
+                  {t('badge.mintButton')}
+                </ButtonV2>
+              )}
+
+              {showCurateButton && (
+                <ButtonV2
+                  backgroundColor={colors.greenLogo}
+                  disabled={address === ownerAddress}
+                  fontColor={colors.blackText}
+                  onClick={() => curate(badgeId)}
+                  sx={{
+                    borderRadius: '10px',
+                    fontSize: '11px !important',
+                    padding: '0.5rem 1rem !important',
+                    height: 'fit-content !important',
+                    lineHeight: '14px',
+                    fontWeight: 700,
+                    boxShadow: 'none',
+                    textTransform: 'uppercase',
+                  }}
+                  variant="contained"
+                >
+                  {t('badge.curateButton')}
+                </ButtonV2>
+              )}
+
+              {showClaimButton && (
+                <ButtonV2
+                  backgroundColor={colors.blue}
+                  disabled={!badgeReviewTimeFinished}
+                  fontColor={colors.white}
+                  onClick={() => handleClaimBadge(badge.id)}
+                  sx={{
+                    width: '100%',
+                    height: 'fit-content !important',
+                    marginTop: '1rem',
+                    padding: '0.5rem 1rem !important',
+                    borderRadius: '10px',
+                    fontSize: '15px !important',
+                    lineHeight: '15px',
+                    fontWeight: 700,
+                    boxShadow: 'none',
+                    textTransform: 'uppercase',
+                  }}
+                  variant="contained"
+                >
+                  {t('badge.claimButton')}
+                </ButtonV2>
+              )}
+            </Box>
+          )}
+          <SafeSuspense>
+            <BadgeOwnerPreview ownerAddress={ownerAddress} />
+          </SafeSuspense>
+        </Box>
+        <SafeSuspense>
+          <BadgeStatusAndEvidence />
+        </SafeSuspense>
+      </Stack>
+    </Box>
   )
 }
 
-export default ViewBadge
-ViewBadge.getLayout = (page: ReactElement) => page
-
-// This value is considered fresh for ten seconds (s-maxage=10).
-// If a request is repeated within the next 10 seconds, the previously
-// cached value will still be fresh. If the request is repeated before 59 seconds,
-// the cached value will be stale but still render (stale-while-revalidate=59).
-//
-// In the background, a revalidation request will be made to populate the cache
-// with a fresh value. If you refresh the page, you will see the new value.
-export async function getServerSideProps({ params, query, res }: GetServerSidePropsContext) {
-  res.setHeader('Cache-Control', 'public, s-maxage=20, stale-while-revalidate=59')
-
-  const badgeId = params?.badgeId
-  const contract = query.contract as string
-  const { chainId, subgraphName } = parsePrefixedAddress(contract)
-
-  const subGraph = isTestnet
-    ? devEndpoints[chainId][subgraphName]
-    : endpoints[chainId][subgraphName]
-
-  // Fetch data from external API
-  const request = await gqlQuery(subGraph, { query: BadgeByIdDocument, variables: { id: badgeId } })
-
-  const badge = request.badge
-  const badgeModel = badge?.badgeModel
-
-  const metadataRequest = await Promise.all([
-    ssrGetContentFromIPFS<BadgeMetadata<BackendFileResponse>>(badge?.uri),
-    ssrGetContentFromIPFS<BadgeModelMetadata<BackendFileResponse>>(badgeModel?.uri),
-  ])
-
-  const badgeMetadata = metadataRequest[0] ? metadataRequest[0].result?.content : null
-  const badgeModelMetadata = metadataRequest[1] ? metadataRequest[1].result?.content : null
-
-  const metaTags = {
-    title: `The Badge dApp - ${badgeModelMetadata?.name} Certificate`,
-    description: `${badgeModelMetadata?.description} - Powered by TheBadge `,
-    imageUrl: badgeMetadata?.image.s3Url,
-  }
-
-  // Pass data to the page via props
-  return {
-    props: {
-      metaTags,
-      badge: {
-        ...badge,
-        badgeModel: {
-          ...badgeModel,
-          badgeModelMetadata,
-        },
-        badgeMetadata,
-      },
-    },
-  }
-}
+export default withPageGenericSuspense(ViewBadge)
