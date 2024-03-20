e2fdf1d9a6fcbe87f5538d45805caf09318045fa
Federico Madoery <fede.madoery@hotmail.com>
2024-03-20 08:25:52 -0300

    feat: Mobile menu (#423)

--- src/providers/web3/utils.ts
+++ src/providers/web3/utils.ts
@@ -2,7 +2,9 @@ import { gnosis, goerli, polygon, polygonMumbai, sepolia } from 'viem/chains'
 
 export const DEFAULT_CHAINS = [sepolia, goerli, polygonMumbai, polygon, gnosis]
 
-export function getValidNetwork(selectedNetworkId: string | `${string}:${string}` | undefined) {
+export function getValidNetwork(
+  selectedNetworkId: string | `${string}:${string}` | number | undefined,
+) {
   return DEFAULT_CHAINS.find(({ id }) => {
     return id === Number(selectedNetworkId)
   })?.id
