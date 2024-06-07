0f2cfdf68c3b2d939fa064a7c16689c08ac38e52
Federico Madoery <fede.madoery@hotmail.com>
2024-06-07 18:14:05 -0300

    Add avalanche setup

--- src/providers/web3/utils.ts
+++ src/providers/web3/utils.ts
@@ -1,13 +1,14 @@
 import { http } from 'viem'
-import { gnosis, goerli, polygon, polygonMumbai, sepolia } from 'wagmi/chains'
+import { avalanche, gnosis, goerli, polygon, polygonMumbai, sepolia } from 'wagmi/chains'
 
-export const DEFAULT_CHAINS = [sepolia, goerli, polygonMumbai, polygon, gnosis] as const
+export const DEFAULT_CHAINS = [sepolia, goerli, polygonMumbai, polygon, gnosis, avalanche] as const
 export const DEFAULT_TRANSPORTS = {
   [sepolia.id]: http(),
   [goerli.id]: http(),
   [polygonMumbai.id]: http(),
   [polygon.id]: http(),
   [gnosis.id]: http(),
+  [avalanche.id]: http(),
 }
 export function getValidNetwork(
   selectedNetworkId: string | `${string}:${string}` | number | undefined,
